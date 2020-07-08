330c5348a1c8f6f6d3c078ca8377d71b3045a9d7
ZL <917257515@qq.com>
2020-07-08 09:22:44 +0800

    少导一个包，补上

--- bailis_gathering/src/main/java/com/bailis/gathering/GatheringApplication.java
+++ bailis_gathering/src/main/java/com/bailis/gathering/GatheringApplication.java
@@ -2,6 +2,7 @@ package com.bailis.gathering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import util.IdWorker;
 
